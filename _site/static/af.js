1cef134da2b9e4dd7e2f170b23179059a810c319
Sharone Zitzman <sharoneis@hotmail.com>
2020-02-25 10:22:40 +0200

    removed everything.me api key

--- _site/static/af.js
+++ _site/static/af.js
@@ -24,12 +24,6 @@ function loadBlogPosts() {
   });
 }
 
-function loadJobs() {
-  var apiUrl = 'https://api.import.io/store/data/b76794af-c9cb-4d26-bc5f-45966514f6da/_query'+
-              '?input/webpage/url='+encodeURIComponent('http://everything.me/jobs')+
-              '&_user=f3e9d602-9b6e-4492-bceb-4f0e2caa96f3'+
-              '&_apikey=f3e9d602-9b6e-4492-bceb-4f0e2caa96f3:LapYuqZmTnPfyUXMdNI2JPXFfavUAKpPv/DmC2zXKsHE14kJftKrz14b3KeDD0d/QgUq5102mTs0fV2yoJsP8w==';
-
   $.getJSON(apiUrl).done(function(data) {
     var source   = $('#jobs-template').html();
     var template = Handlebars.compile(source);
